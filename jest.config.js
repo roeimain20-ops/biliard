b85c3f4ac124d0bfaf91b07249c67852eb767a78
roeimain20-ops <roeimain20@gmail.com>
2026-03-16 02:17:53 +0200

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,53 @@
+module.exports = {
+  rootDir: "../",
+  preset: "ts-jest",
+  transformIgnorePatterns: [
+    // Exclude all node_modules except for chai, jsoncrush, and @tailuge/messaging
+    "node_modules/(?!(chai|jsoncrush|@tailuge/messaging))",
+  ],
+  transform: {
+    // Use SWC for transforming both JavaScript and TypeScript files
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          minify: {
+            compress: false,
+            mangle: false,
+            format: { comments: "all" },
+          },
+          // Enable ESM support in SWC
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+          },
+          target: "es2020",
+        },
+      },
+    ],
+  },
+  coveragePathIgnorePatterns: [
+    "node_modules",
+    "gltf.ts",
+    "webgl.ts",
+    "dom.ts",
+    "shorten.ts",
+    "assets.ts",
+  ],
+  coverageReporters: ["text", "json"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    ".*GLTFExporter": "<rootDir>/test/mocks/gltfexporter.ts",
+    ".*GLTFLoader": "<rootDir>/test/mocks/gltfloader.ts",
+    ".*/sound": "<rootDir>/test/mocks/mocksound.ts",
+    "^@tailuge/messaging$":
+      "<rootDir>/node_modules/@tailuge/messaging/dist/index.js",
+  },
+  // Enable ESM support in Jest
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true, // Enable ESM support in ts-jest
+    },
+  },
+}
